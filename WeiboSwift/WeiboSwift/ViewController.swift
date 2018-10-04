4ecf65e15515a92eba498411d0a8410d47e9ab72
bsnpc6b <Godot-SS.bai@aia.com>
2018-10-04 23:24:32 +0800

    项目初始化-并添加了ignore文件

---
+++ WeiboSwift/WeiboSwift/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  WeiboSwift
+//
+//  Created by g on 2018/10/4.
+//  Copyright © 2018 g. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    var window: UIWindow?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+       view.backgroundColor = UIColor.orange
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
