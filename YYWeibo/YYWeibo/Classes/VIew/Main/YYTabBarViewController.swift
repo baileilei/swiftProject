744e106557324dcdcb646566c13a6f0cf95a7054
bsnpc6b <Godot-SS.bai@aia.com>
2018-10-05 22:08:26 +0800

    设置tabbar上图片和文字颜色的两种方式
    1.UITabBar.appearance().tintColor = UIColor.orange
    2.childController.tabBarItem.selectedImage = UIImage(named: "\(imageName)_selected")?.withRenderingMode(.alwaysOriginal)
            childController.tabBarItem.setTitleTextAttributes([kCTForegroundColorAttributeName as NSAttributedStringKey:UIColor.orange], for: .selected)
            childController.tabBarItem.setTitleTextAttributes([kCTFontAttributeName as NSAttributedStringKey:UIFont.systemFont(ofSize: 14)], for: .selected)

--- YYWeibo/YYWeibo/Classes/VIew/Main/YYTabBarViewController.swift
+++ YYWeibo/YYWeibo/Classes/VIew/Main/YYTabBarViewController.swift
@@ -12,24 +12,28 @@ class YYTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//统一设置tabbar中的颜色
+//        UITabBar.appearance().tintColor = UIColor.orange
+        
+        addChildViewController(childController: YYHomeViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(childController: YYDiscoverViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(childController: YYMessageViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(childController: YYProfileViewController(), title: "我的", imageName: "tabbar_profile")
+    
+    
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func addChildViewController(childController: UIViewController,title:String,imageName:String) {
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.title = title;
+        childController.tabBarItem.selectedImage = UIImage(named: "\(imageName)_selected")?.withRenderingMode(.alwaysOriginal)
+        childController.tabBarItem.setTitleTextAttributes([kCTForegroundColorAttributeName as NSAttributedStringKey:UIColor.orange], for: .selected)
+        childController.tabBarItem.setTitleTextAttributes([kCTFontAttributeName as NSAttributedStringKey:UIFont.systemFont(ofSize: 14)], for: .selected)
+        
+        let nav = UINavigationController(rootViewController: childController)
+        
+        addChildViewController(nav)
     }
-    */
 
 }
