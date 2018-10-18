dbddef44058264084f9cb0bf543a73b6167de3b2
bsnpc6b <Godot-SS.bai@aia.com>
2018-10-18 21:14:06 +0800

    自定义cell，  类中类

--- YYWeibo/YYWeibo/Classes/VIew/Home/View/YYStatusPictureView.swift
+++ YYWeibo/YYWeibo/Classes/VIew/Home/View/YYStatusPictureView.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+private let YYStatusPictureViewCellIdentifier = "YYStatusPictureViewCellIdentifier"
+
+let itemMargin : CGFloat = 5
+
+let itemWidth = (ScreenWidth - 2 * 10 - 2 * itemMargin)/3
+
 class YYStatusPictureView: UICollectionView {
     lazy var messagelabel: UILabel = {
         let label = UILabel(textColor: UIColor.magenta, fontSize: 20, isMultiNumbers: 1)
@@ -24,6 +30,8 @@ class YYStatusPictureView: UICollectionView {
             self.snp_updateConstraints { (make) in
                 make.size.equalTo(size)
             }
+            
+            self.reloadData()
         }
     }
     
@@ -39,9 +47,11 @@ class YYStatusPictureView: UICollectionView {
     
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         let flowLayout = UICollectionViewFlowLayout()
-        
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        flowLayout.minimumLineSpacing = itemMargin
+        flowLayout.minimumInteritemSpacing = itemMargin
         super.init(frame: frame, collectionViewLayout: flowLayout)
-        
+        self.dataSource = self
         setupUI()
     }
     
@@ -50,6 +60,7 @@ class YYStatusPictureView: UICollectionView {
     }
     
     private func setupUI(){
+        self.register(YYStatusPictureViewCell.self, forCellWithReuseIdentifier: YYStatusPictureViewCellIdentifier)
         backgroundColor = RandomColor()
         
         addSubview(messagelabel)
@@ -61,9 +72,7 @@ class YYStatusPictureView: UICollectionView {
     }
     
     private func calcSize(count:Int)->CGSize{
-        let itemMargin : CGFloat = 5
-        
-        let itemWidth = (ScreenWidth - 2 * 10 - 2 * itemMargin)/3
+       
         
         let cols = count > 3 ? 3 :count
         let rows = (count - 1) / 3 + 1
@@ -75,6 +84,60 @@ class YYStatusPictureView: UICollectionView {
         
         
         return CGSize(width: currentWidth, height: currentHeight)
+    }
+    
+}
+
+extension YYStatusPictureView: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return picUrls?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: YYStatusPictureViewCellIdentifier, for: indexPath) as! YYStatusPictureViewCell
+        cell.backgroundColor = RandomColor()
+//        cell.statusPictureInfo = picUrls?[indexPath.item]
+        
+        return cell
+        
+    }
+    
+}
+
+
+class YYStatusPictureViewCell: UICollectionViewCell {
+    var statusPictureInfo: YYStatusPictureInfo?{
+        
+        
+        didSet{
+            
+            if let url = statusPictureInfo?.thumbnail_pic{
+                
+                imgaeView.sd_setImage(with: URL(string: url), placeholderImage: UIImage(named: "time_image_placeholder"), options: .allowInvalidSSLCertificates, completed: nil)
+            }
+        }
+    }
+    
+    
+    private lazy var imgaeView: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "time_image_placeholder"))
+        return view
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        contentView.addSubview(imgaeView)
+        
+        imgaeView.snp_makeConstraints { (make) in
+            make.edges.equalTo(self).offset(UIEdgeInsets.zero)
+        }
     }
     
 }
