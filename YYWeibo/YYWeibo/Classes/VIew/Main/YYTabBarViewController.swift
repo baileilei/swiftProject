d197c8768d842a085c85a2ae75c1d628caab513b
godot <godot-SS.Bai@aia.com>
2018-10-23 15:01:17 +0800

    通过内置的JSON文件初始化控制器。 （可从接口获取）

--- YYWeibo/YYWeibo/Classes/VIew/Main/YYTabBarViewController.swift
+++ YYWeibo/YYWeibo/Classes/VIew/Main/YYTabBarViewController.swift
@@ -33,10 +33,11 @@ class YYTabBarViewController: UITabBarController {
         //命名空间----Swift中如何创建一个类。   如果传一个string， 一个VC的名的话，该怎么初始化控制器呢？
         //从哪里获取命名空间？？？
         //swift中是怎么解决{}嵌套的问题？？？  guard守护机制
-        addChildViewController(childControllerName: "YYHomeViewController", title: "首页", imageName: "tabbar_home")
+//        addChildViewController(childControllerName: "YYHomeViewController", title: "首页", imageName: "tabbar_home")
+        howToUseTryType()
     
     }
-    func addChildViewController(childControllerName: String,title:String,imageName:String) {
+    func addChildViewController(childControllerName: String?,title:String?,imageName:String?) {
         //由于字典中只能存储对象，所以通过一个key从字典中获取一个anyObject类型。 并且如果key写错或者没有对应的值，那么久取不到值。 所以返回值可能有值也可能没值。所以最终类型是anyObject？
         let name: String? = Bundle.main.infoDictionary!["CFBundleExecutable"] as? String
         
@@ -44,8 +45,12 @@ class YYTabBarViewController: UITabBarController {
             YYLog(message: "获取命名空间失败")
             return
         }
-        let cls : AnyClass? = NSClassFromString(nam + "." + childControllerName)
-        YYLog(message: cls)
+        
+        var cls :AnyClass?
+        if let chileVCName = childControllerName {
+            cls = NSClassFromString(nam + "." + chileVCName)
+            YYLog(message: cls)
+        }
         
         
         guard let typeCls = cls as? UIViewController.Type else{
@@ -56,6 +61,12 @@ class YYTabBarViewController: UITabBarController {
         let childVC = typeCls.init()
         YYLog(message: childVC)
         
+        childVC.title = title
+        childVC.tabBarItem.image = UIImage(named: imageName!)
+        
+        let nav = UINavigationController(rootViewController: childVC)
+        
+        addChildViewController(nav)
     }
 //override代表重写父类的方法   Swift支持方法的重载，也就是说只要方法的参数个数或者数据类型不相同，那么系统就会认为两个方法。 
     func addChildViewController(childController: UIViewController,title:String,imageName:String) {
@@ -70,4 +81,20 @@ class YYTabBarViewController: UITabBarController {
         addChildViewController(nav)
     }
 
+    
+    func howToUseTryType() {
+        let path = Bundle.main.path(forResource: "MainVCSettings.json", ofType: nil)
+        let data = NSData(contentsOfFile: path!)!
+        
+        do {
+            let objc : [[String:Any]]? = try! JSONSerialization.jsonObject(with: data as Data, options: .mutableContainers) as? [[String:Any]]
+            for dict in objc! {
+                YYLog(message: dict)
+                addChildViewController(childControllerName: dict["vcName"] as? String, title: dict["title"] as? String, imageName: dict["imageName"] as? String)
+            }
+        } catch {
+            YYLog(message: error)
+        }
+        
+    }
 }
