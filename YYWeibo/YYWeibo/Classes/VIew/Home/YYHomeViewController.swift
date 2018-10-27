0627a684a69dd941909ebbae87122ba7315c68a5
bsnpc6b <Godot-SS.bai@aia.com>
2018-10-27 13:32:14 +0800

    管理动画的对象---动画的锚点

--- YYWeibo/YYWeibo/Classes/VIew/Home/YYHomeViewController.swift
+++ YYWeibo/YYWeibo/Classes/VIew/Home/YYHomeViewController.swift
@@ -64,14 +64,59 @@ class YYHomeViewController: YYVistorTableViewController {
 
 extension YYHomeViewController : UIViewControllerTransitioningDelegate{
     
-//    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-//        <#code#>
-//    }(<#parameters#>) -> <#return type#> {
-//        <#function body#>
-//    }
+    //该方法用于返回一个负责转场动画的对象 ---可以在该对象中控制弹出视图的尺寸等。
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
 //        return presentationController(forPresented:presenting:source:)
         return YYPresentationController(presentedViewController:presented , presenting: presenting)
     }
+    
+    //该方法用于返回一个负责转场如何出现的对象
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning?{
+        return self as! UIViewControllerAnimatedTransitioning
+    }
+    
+    //该方法用于返回一个负责转场如何消失的对象
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning?{
+        
+        return self as! UIViewControllerAnimatedTransitioning
+    }
+}
+
+extension YYHomeViewController : UIViewControllerAnimatedTransitioning{
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 998
+    }
+    
+    //专门用于管理modal如何展现和消失的， 无论是展现还是消失都会调用该方法
+    /*
+     注意点：只要我们实现了这个代理方法，那么系统就不会再有默认的动画了。
+     也就是说，默认的modal从下至上的移动，系统就不再帮我们添加了，所有的动画操作都需要我们自己实现，包括需要展现的视图也需要我们自己添加到容器视图上(containerView)
+     */
+    //transitionContext： 所有动画需要的东西都保存在上下文中， 换而言之，就是可以通过transitionContext获取都我们所关注的视图等。
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
+        let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
+        YYLog(message: toVC)
+        YYLog(message: fromVC)
+        
+        //将需要弹出的视图添加到containerView上
+        guard let toView = transitionContext.view(forKey: UITransitionContextViewKey.to) else {
+            return
+        }
+        
+//        toView.transform = cgaffinetransform(glScalef(<#T##x: GLfloat##GLfloat#>, <#T##y: GLfloat##GLfloat#>, <#T##z: GLfloat##GLfloat#>))
+        toView.transform = CGAffineTransform(scaleX: 1.0,y: 0.0)
+        toView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        //面试题: layer的锚点怎么用的。
+//       UIView.animate(withDuration: <#T##TimeInterval#>, delay: <#T##TimeInterval#>, usingSpringWithDamping: <#T##CGFloat#>, initialSpringVelocity: <#T##CGFloat#>, options: <#T##UIViewAnimationOptions#>, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+        
+        UIView.animate(withDuration: 10.0, delay: 0, options: .allowAnimatedContent, animations: {
+            toView.transform = CGAffineTransform.identity
+            
+        }) { (_) in
+            transitionContext.completeTransition(true)
+        }
+    }
+    
     
 }
