488f94a4842bfabc77f7beda8052ac69c6b8cce2
bsnpc6b <Godot-SS.bai@aia.com>
2018-10-27 10:13:18 +0800

    待解问题：为啥这个button传过来了？ 没有titleBtnClick 冒号

--- YYWeibo/YYWeibo/Classes/VIew/Home/YYHomeViewController.swift
+++ YYWeibo/YYWeibo/Classes/VIew/Home/YYHomeViewController.swift
@@ -19,10 +19,30 @@ class YYHomeViewController: YYVistorTableViewController {
             navigationItem.rightBarButtonItem =  UIBarButtonItem(title: "", target: self, imageName: "navigationbar_pop", action: #selector(swipe))
 //            tabBarItem.title = "首页"
             title = "首页"
+            
+           
+            
             YYLog(message: childViewControllers)
         }else{
             vistorView?.updateVisitorInfo(message: nil, imageName: nil)
         }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let titleBtn : YYTitleButton = YYTitleButton()
+        titleBtn.setTitle("小马哥", for: .normal)
+        titleBtn.sizeToFit()
+        titleBtn.addTarget(self, action: #selector(titleBtnClick), for: .touchUpInside)
+        navigationItem.titleView = titleBtn
+
+    }
+    
+    @objc func titleBtnClick(btn: YYTitleButton){
+        YYLog(message: "haha")
+        btn.isSelected = !btn.isSelected
+        
         
     }
     
